a043bef7e4824d985ca4e134e75414afa61cc6f6
Drenight <2388201351@qq.com>
2019-03-17 22:05:03 +0800

    rooms developed

--- exp2/cookies.py
+++ exp2/cookies.py
@@ -1,11 +1,17 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
 import tornado.options
 import os.path
+import json
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
+
+room = [list() for i in range(100)]		#最大100个聊天室
+inRoom = dict()							#查询usr在哪个聊天室
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -20,33 +26,92 @@ class LoginHandler(BaseHandler):
         self.redirect("/")
 
 class WelcomeHandler(BaseHandler):
-    @tornado.web.authenticated
-    def get(self):
-        self.render('index.html', user=self.current_user)
+	@tornado.web.authenticated
+	def get(self):
+		roomCnt = self.application.rooms.getRooms()
+		self.render('index.html', user=self.current_user,roomCnt=roomCnt)
 
 class LogoutHandler(BaseHandler):
-    def get(self):
-        if (self.get_argument("logout", None)):
-            self.clear_cookie("username")
-            self.redirect("/")
+	def get(self):
+		if (self.get_argument("logout", None)):
+			if self.current_user in inRoom:
+				room[inRoom[self.current_user]].remove(self.current_user)
+			
+			self.clear_cookie("username")
+			self.redirect("/")
+##############################################################################
+
+class Rooms(object):
+	cnt = 0
+	callbacks = []
+	def register(self,callback):
+		self.callbacks.append(callback)
+	def unregister(self,callback):
+		self.callbacks.remove(callback)
+	def addRoom(self):
+		self.cnt+=1
+		self.notifyCallbacks()
+
+	def notifyCallbacks(self):
+		for callback in self.callbacks:
+			callback(self.getRooms())
+	def getRooms(self):
+		return self.cnt
+
+class RoomsHandler(tornado.web.RequestHandler):
+	def post(self):
+		action = self.get_argument('action')
+		
+		if action == 'add':
+			self.application.rooms.addRoom()
+		else:
+			self.set_status(400)
+		
+class StatusHandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		self.application.rooms.register(self.callback)
+	def on_close(self):
+		self.application.rooms.unregister(self.callback)
+	def on_message(self,message):
+		pass
+	def callback(self,count):
+		self.write_message('{"roomCnt":"%d"}' % count)	
+
+#############################################################################
+
+class JoinHandler(BaseHandler):
+	def get(self):
+		roomid = self.get_argument("roomid",0)
+		room[roomid].append(self.current_user)
+
+		self.render('chatroom.html',user=self.current_user)
+
+class Application(tornado.web.Application):
+	def __init__(self):
+		self.rooms = Rooms()
+		
+		settings = {
+			"template_path": os.path.join(os.path.dirname(__file__), "templates"),
+			"static_path": 'static',
+			"cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
+			"xsrf_cookies": True,
+			"login_url": "/login"
+		}
+		handlers = [
+			(r'/', WelcomeHandler),
+			(r'/login', LoginHandler),
+			(r'/logout', LogoutHandler),
+			(r'/rooms',RoomsHandler),
+			(r'/status',StatusHandler),
+			(r'/join',JoinHandler),
+		]
+
+		tornado.web.Application.__init__(self,handlers,**settings)
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
-
-    settings = {
-        "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-        "static_path": 'static',
-		"cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
-		"xsrf_cookies": True,
-        "login_url": "/login"
-    }
-
-    application = tornado.web.Application([
-        (r'/', WelcomeHandler),
-        (r'/login', LoginHandler),
-        (r'/logout', LogoutHandler)
-    ], **settings)
-
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+	tornado.options.parse_command_line()
+	application = Application()
+	
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
--- exp2/static/scripts/chatrooms.js
+++ exp2/static/scripts/chatrooms.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	xsrf = getCookie("_xsrf");
-	
-	$('#logout').click(function(event){
 
+	setTimeout(addRoom,100);
+
+	$('#logout').click(function(event){
 		jQuery.ajax({
 			url:'//localhost:8000/logout',
 			type:'GET',
@@ -12,18 +13,58 @@ $(document).ready(function(){
 				logout: 'logout'
 			},
 			datatype: 'json',
-
 			success: function(data,status,xhr){
 				window.location.href='http://localhost:8000/login'
 			}
 		});	
-
 	});
-		
+
+	$('#createRoom').click(function(event){
+		jQuery.ajax({
+			url:'//localhost:8000/rooms',
+			type:'POST',
+			data:{
+				_xsrf: xsrf,
+				roomCnt:document.roomCnt,
+				action:'add'
+			},	
+			dataType:'json',
+
+			beforeSend: function(xhr,settings){
+				$(event.target).attr('disabled','disabled');
+			},
+			success: function(data,status,xhr){
+				window.location.href='http://localhost:8000/'
+			}
+		});
+	});
 });
+
+
+
+//function requestRooms(){
+//	jQuery.getJSON('//localhost:8000/qRoom')
+	
+//}
 
 function getCookie(name){
 	var c = document.cookie.match("\\b"+name+"=([^;]*)\\b");
 	return c ? c[1]:undefined;
+}
+function addRoom(cnt){
+	if(cnt==100){
+		return;
+	}
+	var host = 'ws://localhost:8000/status'
+	var websocket = new WebSocket(host);
+
+	websocket.onopen = function(evt){ };
+	websocket.onmessage = function(evt){
+		$('#roomCnt').html($.parseJSON(evt.data)['roomCnt']);
+	};
+	websocket.onerror = function(evt){ };
+}
+function join(){
+	
 }
 
